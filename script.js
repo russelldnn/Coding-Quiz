89ba382ec81d0159d771ee800e8c8acbcb1a2727
russelldnn <russelldnn@gmail.com>
2022-05-25 19:31:31 -0400

    got the timer working

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ let questions = [
         answer: "flat brackets"
     },
 ]
+
+let score = 0;
+
+let initialBox = document.getElementById("initialBox");
+let timer = document.getElementById("timer");
+let startButton = document.getElementById("startButton");
+let questionBox = document.getElementById("questionBox");
+let promptList = document.getElementById("promptList");
+
+let timeLeft = 30;
+let penaltyTime = 10;
+
+startButton.addEventListener("click", function(){
+    let timerInterval = setInterval(function() {
+    timeLeft--;
+    timer.textContent = "Time left:" + timeLeft;
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            timer.textContent = "Time is up sorry!";
+        }
+}, 1000);
+}
+    
+)
