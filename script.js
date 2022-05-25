8f6a51b1d80a17c61224c2b5a98e444afdbdd567
russelldnn <russelldnn@gmail.com>
2022-05-25 19:04:29 -0400

    added an array of questions and answers to script

---
+++ script.js
@@ -0,0 +1,22 @@
+let questions = [
+    {
+        quest: "Which is not a flex direction?",
+        choice: ["row", "row-Reverse", "column", "inverse"],
+        answer: "inverse"
+    },
+    {
+        quest: "What does concatenate do?",
+        choice: ["joins", "deletes", "moves", "segments"],
+        answer: "joins"
+    },
+    {
+        quest: "What is the most common data type of the following?",
+        choice: ["bigINT","Null","string", "Read-Only"],
+        answer: "string"
+    },
+    {
+        quest: "Arrays are enclosed within?",
+        choice: ["commas","flat brackets", "curly brackets","pound signs"],
+        answer: "flat brackets"
+    },
+]
